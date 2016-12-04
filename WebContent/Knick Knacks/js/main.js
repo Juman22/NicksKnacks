8c1a3fbc42a054170c639db7fb37de35313b785d
bliu93 <billy.liu9@gmail.com>
2016-12-04 17:24:18 -0500

    Adding current state of front end

---
+++ WebContent/Knick Knacks/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('.fadeshop').hover(
+		function(){
+			$(this).find('.captionshop').fadeIn(150);
+		},
+		function(){
+			$(this).find('.captionshop').fadeOut(150);
+		}
+	);
+});
