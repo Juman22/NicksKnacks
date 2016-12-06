65066e8b6caacf1dd47ba9e46ae4d61ffbc61b82
bliu93 <billy.liu9@gmail.com>
2016-12-06 01:52:48 -0500

    Update main.js

--- WebContent/Knick Knacks/js/main.js
+++ WebContent/Knick Knacks/js/main.js
@@ -7,4 +7,13 @@ $(document).ready(function() {
 			$(this).find('.captionshop').fadeOut(150);
 		}
 	);
+	
+	 
+    //delete items of a cart in checkout page
+    $("#edd_checkout_cart").on("click","a", function(){
+        $(this).closest("tr").fadeOut(500, function(){
+            $(this).remove();
+        });
+        
+    });
 });
