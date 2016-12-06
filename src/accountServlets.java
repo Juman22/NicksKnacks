4ffcaf4a6e2f5e663c41ab2e9b5103e5cb3f764d
shivyanish25 <shivyanish25@gmail.com>
2016-12-06 05:11:47 -0500

    Add files via upload

---
+++ src/accountServlets.java
@@ -0,0 +1,94 @@
+package mainPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.uga.cs4300.persistlayer.*;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateExceptionHandler;
+import queryLayer.queryLogic;
+
+/**
+ * Servlet implementation class accountServlets
+ */
+@WebServlet("/accountServlets")
+public class accountServlets extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	queryLogic queryLayerMain = new queryLogic();
+	
+    Configuration cfg = null;
+    
+    private String templateDir = "/WEB-INF/templates";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public accountServlets() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void init() {
+        // Create your Configuration instance, and specify if up to what FreeMarker
+        // version (here 2.3.25) do you want to apply the fixes that are not 100%
+        // backward-compatible. See the Configuration JavaDoc for details.
+        cfg = new Configuration(Configuration.VERSION_2_3_25);
+        
+        // Specify the source where the template files come from.
+        cfg.setServletContextForTemplateLoading(getServletContext(), templateDir);
+        
+        // Sets how errors will appear.
+        // During web page *development* TemplateExceptionHandler.HTML_DEBUG_HANDLER is better.
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        
+        // Don't log exceptions inside FreeMarker that it will thrown at you anyway:
+        cfg.setLogTemplateExceptions(false);
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+        
+        Map<String, Object> root = new HashMap<>();
+        
+        
+        
+        queryLayerMain.getAccountInfo(root, "dwightschrute");
+
+        queryLayerMain.deployProfile(response, root, cfg);
+ 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+
+
+		 
+
+		}
+	}
+
