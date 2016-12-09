71d35b2c01a6c0b801393001c27c5a8bd086cc97
bliu93 <billy.liu9@gmail.com>
2016-12-08 22:04:50 -0500

    added comments regarding getting a row's info

--- WebContent/Knick Knacks/js/main.js
+++ WebContent/Knick Knacks/js/main.js
@@ -35,21 +35,10 @@ $(document).ready(function() {
         });
     });	
     
-    $('#addToCart').click(function() {    
-        var params = $('span.product-prop');
-        var quantitySelected = $(".qty-product").val();    
-        var product = {
-            "name": params.eq(0).text(),
-            "description": params.eq(1).text(),
-            "skuNum": params.eq(2).text(),
-	    "price":params.eq(3).text(),
-	    "quantity": quantitySelected	
-        };
-        $.ajax({
-            url: "/NicksKnacks/cartServlet?",
-            type: "GET",
-            data: product, 
-            success: function(result) {console.log("Successful");}
-        });
-    });
+    /*Grabbing the info of a row
+    var cartRow = $(".edd_cart_remove_item_btn").closest("tr");
+    var productName = cartRow.find(".cart-item-name").text();
+    var productPrice = cartRow.find(".edd_cart_item_price").text();
+    var productQuantity = cartRow.find("td input").val();
+    */
 });
